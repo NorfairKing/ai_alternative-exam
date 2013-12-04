9e583ac8a08f5c4c57fc4a12e8c2f9e436c3b50b
Tom Sydney Kerckhove <Syd.Kerckhove@student.kuleuven.be>
2013-12-04 17:39:19 +0100

    corrected the version spaces algorithm

---
+++ resources/graphs/compiledot.sh
@@ -0,0 +1,7 @@
+dot -Tpng iteration_1.dot -o iteration_1.png
+dot -Tpng iteration_2.dot -o iteration_2.png
+dot -Tpng iteration_3.dot -o iteration_3.png
+dot -Tpng iteration_4.dot -o iteration_4.png
+dot -Tpng iteration_5.dot -o iteration_5.png
+dot -Tpng resultaat.dot -o resultaat.png
+dot -Tpng uitwerking.dot -o uitwerking.png
