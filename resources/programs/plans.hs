eb3a11bd6be170e420771738c07c3ac4118c94e3
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2013-12-15 07:36:29 +0100

    first draft of question 4 done

---
+++ resources/programs/plans.hs
@@ -0,0 +1,16 @@
+import Data.List
+import Data.Maybe
+
+be :: Eq a => a -> a -> [a] -> Bool
+be a b l = ((elemIndex a l)) < ( ( elemIndex b l))
+
+p = ["NE","NS","BE","BS","DSO"]
+
+plans = [[a,b,c,d,e] | a <- p, b <- p, c <- p, d <- p, e <- p, a /= b, a /= c, a /= d, a /= e, b /= c, b /= d, b /= e, c /= d, c /= e, d /= e]
+
+possible = [ p | p <- plans, be "NE" "BE" p, be "NS" "BS" p, be "BS" "DSO" p]
+
+strings = [ "BEGIN \\rightarrow HEEN \\rightarrow "++a++" \\rightarrow "++b++" \\rightarrow "++c++" \\rightarrow "++d++" \\rightarrow "++e++"\\rightarrow LBO \\rightarrow TERUG \\rightarrow EINDE\\\\" | [a,b,c,d,e] <- possible]
+
+main = do
+    mapM_ putStrLn strings
